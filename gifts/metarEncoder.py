5a38fc701d2f302d39d2c6b6f128e35e0b002786
Mark Oberfield <Mark.Oberfield@gmail.com>
2025-12-29 15:29:41 -0500

    Added GPL license text to doc body

--- gifts/metarEncoder.py
+++ gifts/metarEncoder.py
@@ -2,9 +2,22 @@
 # Name: metarEncoder.py
 # Purpose: To encode METAR/SPECI information in IWXXM XML format.
 #
-# Author: Mark Oberfield
-# Organization: NOAA/NWS/OSTI/Meteorological Development Laboratory
-# Contact Info: Mark.Oberfield@noaa.gov
+# Copyright (C) 2025 Mark Oberfield
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Contact Info: Mark.Oberfield@gmail.com
 #
 import logging
 import re
